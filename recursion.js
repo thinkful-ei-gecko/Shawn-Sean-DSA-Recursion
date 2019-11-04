bdafb3c3a578cab27ff4e039afb7ab4db7a1001c
Sean Cooper <seankamran@gmail.com>
2019-11-04 22:18:46 +0000

    first commit

--- recursion.js
+++ recursion.js
@@ -19,3 +19,32 @@ function powerCalculator(base, exp){
 }
 
 powerCalculator(2, 5);
+
+
+
+function factorial(n){
+  if (n===0){
+    return 1
+  }
+  return n*factorial(n-1)
+  }
+  
+  factorial(5)
+
+
+  function fibonacci(n){
+    if(n===0){
+     return 0 
+    }
+    if(n===1){
+     return 1
+    }
+     return fibonacci(n-1) + fibonacci(n-2)
+  }
+   
+fibonacci(7)
+
+
+
+
+
